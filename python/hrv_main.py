8fc5a7c56fa0b001a83f159b36b3b7d77fae892a
HanoolMoon <gksnf21@gmail.com>
2026-04-06 17:51:29 +0900

    0406-Moon-머신러닝 모델 올림

--- python/hrv_main.py
+++ python/hrv_main.py
@@ -9,7 +9,7 @@ import joblib
 
 # === ML 모델 로드 ===
 try:
-    ml_model = joblib.load('models/health_metrics_model.pkl')
+    ml_model = joblib.load('C:/Users/smhrd/Desktop/rf_model (1).pkl')
 except:
     ml_model = None
 
